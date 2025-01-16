a4d16d07270897e1a1f59949838520e8e3eedafd
Renan <renanmendes421@icloud.com>
2025-01-16 00:44:07 -0300

    Code refactoring + Brazilian Portuguese word database

--- js/Term.js
+++ js/Term.js
@@ -1,150 +1,191 @@
+import { data } from "../words_data.js";
+
 class Term {
-  constructor(display) {
-    this.display = display;
-    this.display.forEach((v) => {
-      v.setAttribute("isCompleted", false);
-    });
-
-    this.word;
+  constructor(displays, boxes) {
+    this.displays = displays;
+    this.boxes = boxes;
+    this.secretWord = this.chosenSecretWord();
+    this.currentRow = 0;
+    this.currentCol = 0;
+    this.modal = document.getElementById("modal");
+    this.modalTitle = document.getElementById("modal-title");
+    this.modalText = document.getElementById("modal-text");
+    this.repeatButton = document.getElementById("repeatButton");
+    this.backButton = document.getElementById("backButton");
+    this.init();
   }
 
-  guessWord(words) { // Get a random word to be the guess word
-    const randomIndex = Math.floor(Math.random() * words.length);
-    this.word = words[randomIndex];
+  init() {
+    document.addEventListener("keydown", (e) => this.handleKeyPress(e));
+    console.log(this.secretWord);
+    this.repeatButton.addEventListener("click", () => this.restartGame());
+    this.backButton.addEventListener("click", () => window.location.href = "/index.html");
+    this.addBoxClickListeners();
+    this.updateBoxFocus();
   }
 
-  whichDisplay() {
-    // Return the current display index
-    let index;
+  randomWord() {
+    const randomIndex = Math.floor(Math.random() * data.length);
+    return data[randomIndex];
+  }
 
-    for (let i = 0; i < this.display.length; i++) {
-      for (let j = 0; j < this.display[i].children.length; j++) {
-        if (this.display[i].getAttribute("isCompleted") == "false") {
-          index = i;
-          i = this.display.length - 1;
-          j = this.display[i].children.length - 1;
-        }
-      }
+  chosenSecretWord() {
+    let word = this.randomWord();
+
+    if (word.length === 5) {
+      return word;
+    } else {
+      return this.chosenSecretWord();
     }
 
-    return index;
   }
 
-  whichBox() {
-    // Return the current box index
-    let currentDisplay = this.whichDisplay();
-
-    for (let i = 0; i < this.display[currentDisplay].children.length; i++) {
-      if (
-        this.display[currentDisplay].children[i].style.borderBottom ==
-        "8px solid rgb(62, 62, 62)"
-      ) {
-        return i;
-        i = this.display[currentDisplay].children.length - 1;
-      }
-    }
-  }
-
-  boxFocus(displayIndex, boxIndex) {
-    // Style the box which is focused
-    this.display[displayIndex].children[boxIndex].style.borderBottom =
-      "8px solid #3e3e3e";
-  }
-
-  boxUnfocus(displayIndex) {
-    // Unstyle the box which's focused
-    for (let j = 0; j < this.display[displayIndex].children.length; j++) {
-      this.display[displayIndex].children[j].style.borderBottom =
-        "5px solid #3e3e3e";
+  handleKeyPress(e) {
+    if (e.key === "Enter") {
+      this.gameOver(this.checkGuess());
+    } else if (e.key === "Backspace") {
+      this.removeLetter();
+    } else if (e.key === "ArrowRight") {
+      this.moveRight();
+    } else if (e.key === "ArrowLeft") {
+      this.moveLeft();
+    } else if (e.key.length === 1 && e.key.match(/[a-z]/i)) {
+      this.addLetter(e.key);
     }
   }
 
-  boxClick() {
-    // Select the box clicked by the user
-    let displayIndex = this.whichDisplay();
-
-    for (let i = 0; i < this.display[displayIndex].children.length; i++) {
-      this.display[displayIndex].children[i].addEventListener("click", () => {
-        this.boxUnfocus(displayIndex);
-        this.boxFocus(displayIndex, i);
-      });
+  addLetter(letter) {
+    if (this.currentCol < 5) {
+      const box = this.displays[this.currentRow].children[this.currentCol];
+      box.textContent = letter;
+      this.currentCol++;
+      this.updateBoxFocus();
     }
-  }
-
-  checkChars(e) {
-    const chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOEPQRSTUVWXYZ";
-
-    for (let i = 0; i < chars.length; i++) {
-      if (e == chars[i]) {
-        i = chars.length - 1;
-        return true;
-      }
-    }
-    return false;
-  }
-
-  addLetter(e) {
-    let displayIndex = this.whichDisplay();
-    let boxIndex = this.whichBox();
-    let isLetter = this.checkChars(e);
-
-    if (isLetter == true) {
-      this.display[displayIndex].children[boxIndex].innerText = e.toUpperCase();
-      this.nextBox();
-    }
-
   }
 
   removeLetter() {
-    let displayIndex = this.whichDisplay();
-    let boxIndex = this.whichBox();
+    if (this.currentCol > 0) {
+      this.currentCol--;
+    }
+    const box = this.displays[this.currentRow].children[this.currentCol];
+    if (box && box.textContent !== "") {
+      box.textContent = "";
+    }
+    this.updateBoxFocus();
+  }
 
-    if (this.display[displayIndex].children[boxIndex].innerText != "") {
-      this.display[displayIndex].children[boxIndex].innerText = "";;
-    } else if (this.display[displayIndex].children[boxIndex].innerText == "") {
-      this.previousBox();
-      boxIndex = this.whichBox();
-      this.display[displayIndex].children[boxIndex].innerText = "";
+  moveRight() {
+    if (this.currentCol < 5) {
+      this.currentCol++;
+      this.updateBoxFocus();
     }
   }
 
-  nextBox() {
-    // Go to the next focused box
-    let currentDisplay = this.whichDisplay();
-    let currentBox = this.whichBox();
-
-    this.boxUnfocus(currentDisplay);
-
-    if (currentBox < this.display[currentDisplay].children.length - 1) {
-      this.boxFocus(currentDisplay, currentBox + 1);
-    } else if (currentBox == this.display[currentDisplay].children.length - 1) {
-      this.boxFocus(currentDisplay, currentBox);
+  moveLeft() {
+    if (this.currentCol > 0) {
+      this.currentCol--;
+      this.updateBoxFocus();
     }
   }
 
-  previousBox() {
-    // Go to the previous focused box
-    let currentDisplay = this.whichDisplay();
-    let currentBox = this.whichBox();
-
-    this.boxUnfocus(currentDisplay);
-
-    if (
-      currentBox > 0 &&
-      currentBox <= this.display[currentDisplay].children.length - 1
-    ) {
-      this.boxFocus(currentDisplay, currentBox - 1);
-    } else if (currentBox == 0) {
-      this.boxFocus(currentDisplay, 0);
+  updateBoxFocus() {
+    if (!this.displays[this.currentRow]) return; // Add this line to check if the current row exists
+    for (let i = 0; i < 5; i++) {
+      const box = this.displays[this.currentRow].children[i];
+      if (i === this.currentCol) {
+        box.classList.add("focused");
+        box.classList.remove("unfocused");
+      } else {
+        box.classList.add("unfocused");
+        box.classList.remove("focused");
+      }
     }
+  }
+
+  gameOver(result) {
+    if (result.every(color => color === 'green')) {
+      this.showModal("Você ganhou!", `A palavra secreta era ${this.secretWord}`);
+      return;
+    }
+
+    if (this.currentRow == 6) {
+      this.showModal("Game over!", `A palavra secreta era ${this.secretWord}`);
+    }
+  }
+
+  showModal(title, text) {
+    this.modalTitle.textContent = title;
+    this.modalText.textContent = text;
+    this.modal.classList.remove("hidden");
+  }
+
+  restartGame() {
+    window.location.reload();
   }
 
   checkGuess() {
-
+    if (this.currentCol === 5) {
+      const guess = Array.from(this.displays[this.currentRow].children).map(box => box.textContent).join("");
+      const result = this.checkGuessResult(guess, this.secretWord);
+      this.colorBoxes(result);
+      this.currentRow++;
+      this.currentCol = 0;
+      this.updateBoxFocus(); // Add this line to update the focus
+      return result;
+    }
   }
 
-  print() {
-    console.log(this.word);
+  checkGuessResult(guess, secret) {
+    const result = [];
+    const secretLetters = secret.split('');
+    const guessLetters = guess.split('');
+
+    guessLetters.forEach((letter, index) => {
+      if (letter === secretLetters[index]) {
+        result[index] = 'green';
+        secretLetters[index] = null;
+      } else {
+        result[index] = 'grey';
+      }
+    });
+
+    guessLetters.forEach((letter, index) => {
+      if (result[index] === 'grey' && secretLetters.includes(letter)) {
+        result[index] = 'yellow';
+        secretLetters[secretLetters.indexOf(letter)] = null;
+      }
+    });
+
+    return result;
+  }
+
+  colorBoxes(result) {
+    for (let i = 0; i < 5; i++) {
+      const box = this.displays[this.currentRow].children[i];
+      if (result[i] === 'green') {
+        box.style.backgroundColor = "green";
+      } else if (result[i] === 'yellow') {
+        box.style.backgroundColor = "blue";
+      } else {
+        box.style.backgroundColor = "";
+      }
+    }
+  }
+
+  addBoxClickListeners() {
+    for (let row = 0; row < this.displays.length; row++) {
+      for (let col = 0; col < this.displays[row].children.length; col++) {
+        const box = this.displays[row].children[col];
+        box.addEventListener("click", () => this.handleBoxClick(row, col));
+      }
+    }
+  }
+
+  handleBoxClick(row, col) {
+    if (row === this.currentRow) {
+      this.currentCol = col;
+      this.updateBoxFocus();
+    }
   }
 }
 
--- js/runTerm.js
+++ js/runTerm.js
@@ -1,37 +1,18 @@
 import Term from "./Term.js";
 
-function runTerm() {
+function main() {
+  const displays = document.getElementById("container-displays").getElementsByClassName("display");
+  const boxes = document.querySelectorAll(".box");
+  const term = new Term(displays, boxes);
+  clickEvent();
+}
+
+function clickEvent() {
   const title = document.getElementById("title");
   const otherGameButton = document.getElementById("otherGameButton");
-  const displays = document.querySelectorAll(".display");
-
+  
   title.addEventListener("click", () => window.location.href = "/index.html");
   otherGameButton.addEventListener("click", () => window.location.href = "/index.html");
-
-  const term = new Term(displays);
-  const words = ["carro", "abaco", "bebem", "folha", "melão", "piano", "nuvem", "sonho", "velho", "festa"];
-  term.guessWord(words);
-  term.print();
-  term.boxFocus(0, 0);
-  term.boxClick();
-  document.addEventListener("keydown", e => {
-    term.addLetter(e.key);
-
-  });
-  document.addEventListener("keydown", e => {
-    if (e.key == "Backspace") {
-      term.removeLetter();
-    }
-  });
-  document.addEventListener("keydown", e => {
-    if (e.key == "ArrowLeft") {
-      term.previousBox();
-    }
-  });
-  document.addEventListener("keydown", e => {
-    if (e.key == "ArrowRight") {
-      term.nextBox();
-    }
-  });
 }
-runTerm();
+
+main();
